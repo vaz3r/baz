ec06493967c1f2830f412b99394f9cc1cd97e9ed
Vazer Rahmani <vaz3r@outlook.com>
2018-09-20 20:03:03 +0100

    v3.5

--- core.py
+++ core.py
@@ -5,15 +5,13 @@ import time
 
 with open('action.json') as f:
     jsondata = json.load(f)
-
+start = time.time()
 for data in jsondata["data"]:
-    start = time.time()
     print(data["title"])
     page = requests.get("https://www.youtube.com/results?search_query=" + data["title"] + " trailer")
     soup = BeautifulSoup(page.content, 'html.parser')
     trailer_html = soup.find("h3", class_="yt-lockup-title")
     trailer = trailer_html.find("a")["href"]
     print(trailer)
-    end = time.time()
-    print(end - start)
-    break
+end = time.time()
+print(end - start)
